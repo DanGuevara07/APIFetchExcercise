0f870fac44f8f7542345e0b4899cfc6182fc1482
DanGuevara07 <guevaramdan@gmail.com>
2023-05-05 17:03:48 -0500

    Last commit, 3 Fetch API finished, might add new functions later

--- assets/js/creditCard.js
+++ assets/js/creditCard.js
@@ -1,8 +1,12 @@
 
 const randomCard = document.querySelector('#randomCard');
 const mainSection = document.querySelector('#cardDiv');
-const baseurl = 'https://restcountries.com/v3.1/all'
-let countryInfo = {};
+const personNameUrl = 'https://api.generadordni.es/v2/profiles/person?results=1&include_fields=fullname'
+const bankUrl = 'https://api.generadordni.es/v2/bank/account?results=1&include_fields=entity';
+const creditCardUrl = 'https://api.generadordni.es/v2/bank/card?results=1';
+let cardInfo = {};
+let bankName = '';
+let personName = '';
 
 function generateRandom(min = 1, max = 250) {
 
@@ -21,11 +25,33 @@ function generateRandom(min = 1, max = 250) {
     return rand;
 }
 
-async function getNewCountry(){
-    await fetch(`${baseurl}`)
+async function getNewCreditCard(){
+    await fetch(`${creditCardUrl}`)
     .then(async (data)=> {
         await data.json().then((json)=>{
-            countryInfo = json[generateRandom()];
+            cardInfo = json[0];
+        })
+    })
+    .catch(error =>{
+        console.log(error); 
+    })
+}
+async function getNewBank(){
+    await fetch(`${bankUrl}`)
+    .then(async (data)=> {
+        await data.json().then((json)=>{
+            bankName = json[0].entity;
+        })
+    })
+    .catch(error =>{
+        console.log(error); 
+    })
+}
+async function getNewPerson(){
+    await fetch(`${personNameUrl}`)
+    .then(async (data)=> {
+        await data.json().then((json)=>{
+            personName = json[0].fullname;
         })
     })
     .catch(error =>{
@@ -33,45 +59,39 @@ async function getNewCountry(){
     })
 }
 
-function printRandomCountrySection(){
-    mainSection.innerHTML = `
-    <div id="cardDiv" class="card w-50">
-        <img src="https://thumbs.dreamstime.com/b/international-business-world-flags-globe-travel-services-management-concept-vector-eps-54034810.jpg" class="card-img-top" alt="Country">
-        <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Click the button below to get a random country</h5>
-        </div>
-    </div>
-    <a href="#" class="btn btn-primary mt-3" id="refresh">Get a New Country</a>
-    `
-    document.querySelector('#refresh').addEventListener('click',async () => {
-        await getNewCountry();
-        console.log(countryInfo);
-        printRandomCountry();
-        console.log(Object.values(countryInfo.currencies));
-        
-    })
-
-}
 
 function printRandomCard(){
-    // <p class="card-text">Currency: ${Object.values(Object.values(countryInfo.currencies))}</p>
     document.querySelector('#cardDiv').innerHTML = `
-        <img src="${countryInfo.flags.png}" class="card-img-top" alt="Country">
-        <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Country Found</h5>
-            <p class="card-text">Official Name: ${countryInfo.name.official}</p>
-            <p class="card-text">Capital City: ${countryInfo.capital}</p>
-            <p class="card-text">Region: ${countryInfo.region}</p>
-            <p class="card-text">Subregion: ${countryInfo.subregion}</p>
-            <p class="card-text">Population: ${countryInfo.population} habitants</p>
-            <p class="card-text">Languages: ${Object.values(countryInfo.languages)}</p>
-            <p class="card-text">Flag Description: ${countryInfo.flags.alt}</p>
-            <a href="${countryInfo.maps.googleMaps}">Maps Link</a>
+    <div class="d-flex gap-5 p-5 justify-content-center">
+        <div class="bg-primary rounded d-flex flex-column justify-content-center p-4 text-white" style="width: 35%; height: 280px;">
+            <h2 class="align-self-center mt-4" >${bankName}</h2>
+            <div class="d-flex justify-content-between px-2 align-items-center">
+                <img src="https://cdn-icons-png.flaticon.com/512/680/680284.png" alt="chip" width="50px" height="50px">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgGT9YUYOVlCW9Q8ITK4I3YGYYNp1tmNhnQg&usqp=CAU" alt="mapamundi" width="50px" height="39px">
+            </div>
+            <h1 class="align-self-center mt-3">${cardInfo.card_formatted}</h1>
+            <div class="d-flex justify-content-between px-5 mx-5">
+                <p>Expires ${cardInfo.expiration_date}</p>
+                <p>${cardInfo.name}</p>
+            </div>
+            <p class="ps-5 mt-3" >${personName}</p>
         </div>
+        <div class="bg-primary rounded d-flex flex-column align-items-center" style="width: 35%; height: 280px;">
+            <div class="bg-dark w-100 mt-4" style="height: 15%;" ></div>
+            <div class="bg-secondary mt-3 d-flex justify-content-end align-items-center" style="width: 80%; height: 15%;">
+                <span class="text-white me-2" >CVV</span>
+                <div class="bg-white align-self-end h-100 d-flex justify-content-center align-items-center" style="width: 15%;">${cardInfo.cvc}</div>
+            </div>
+            <div class="p-4 text-white mt-5" style="font-size: 10px; width: 80%;"> This card is non-transferable and for the exclusive use of the holder, in accordance with the terms and conditions that govern its use.
+            This card is issued by ${bankName} in accordance with the ${cardInfo.name} international license.</div>
+        </div>
+    </div>
     `
 }
 
 randomCard.addEventListener('click',async ()=>{
-    console.log('Hello');
-    // printRandomCountrySection();
+    await getNewCreditCard();
+    await getNewBank();
+    await getNewPerson();
+    printRandomCard();
 })
